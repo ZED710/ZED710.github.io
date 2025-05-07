6c13ddfb25948edeb4c89fc8f36f2a6c30f1308c
ZED710 <1411651099@qq.com>
2025-05-07 22:30:12 +0800

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -3,7 +3,7 @@ define({ entries : {
         "author": "Ren, Zeyu and Kong, Xiangyu and Zhang, Yudong and Wang, Shuihua",
         "doi": "10.1109/OJEMB.2023.3305190",
         "journal": "IEEE Open Journal of Engineering in Medicine and Biology",
-        "keywords": "Biomedical imaging;Image augmentation;Semisupervised learning;Semantics;Self-supervised learning;Deep learning;Image analysis;Image classification;Deep learning;self-supervised learning;medical image analysis;semi-supervised learning;image classification",
+       keywords = {methods: Conflict graph, methods: Mixed integer programming, benchmarks:ITC2019, classes:UTT},
         "pages": "459-466",
         "title": "UKSSL: Underlying Knowledge Based Semi-Supervised Learning for Medical Image Classification",
         "type": "ARTICLE",
