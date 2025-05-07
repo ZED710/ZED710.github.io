59464552ad9a401a70886414f9683a8aad8c25c1
ZED710 <1411651099@qq.com>
2025-05-07 20:47:34 +0800

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,11 +1,5 @@
 define({
 	tagCategories: {
-		"methods": {
-			"description": "methods of the paper" 
-		},
-		"benchmarks": {
-			"description": "benchmarks of the paper" 
-		},
 		"classes": {
 			"description": "classifications of the paper" 
 		}
