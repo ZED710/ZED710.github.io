328795d395532e4a4be3d65f2a8747d574dc4066
ZED710 <1411651099@qq.com>
2025-05-07 22:43:57 +0800

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,12 +1,6 @@
 define({
-	%% 以下是在网页Keywords里会显示的内容 %%
 	tagCategories: {
-		"methods": {
-			"description": "methods of the paper" 
-		},
-		"benchmarks": {
-			"description": "benchmarks of the paper" 
-		},
+		
 		"classes": {
 			"description": "classifications of the paper" 
 		}
